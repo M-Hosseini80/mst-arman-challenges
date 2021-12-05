98476e5bacd8c81dfd8332d8933666524204cdb0
seyed <M_Hosseini80@yahoo.com>
2021-12-05 10:41:38 +0000

    second project, done!

--- src/components/Get-repeated-info.js
+++ src/components/Get-repeated-info.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-export default function GetRepeatedInfo(number) {
-    let result = null
-    return (
-        <div>
+export default function GetRepeatedInfo({ number }) {
+    let result = {}
+    // check numbers
+    number.split("").map((eachNum) => {
+        result[eachNum] = ""
 
-        </div>
-    )
+        // if we have repeated numbers 
+        if (number.split(eachNum).length - 1 <= 1) {
+            for (var x = 0; x < eachNum; x++) {
+                result[eachNum] += String(eachNum);
+            }
+        } else {
+            for (var i = 0; i <= eachNum * number.split(eachNum).length - 1 - eachNum; i++) {
+                result[eachNum] += String(eachNum);
+            }
+        }
+        return result;
+
+    });
+    result = JSON.stringify(result)
+    console.log('[change numbers in App.js] |', result)
+    return (<h3>2. {result}</h3>);
 }
